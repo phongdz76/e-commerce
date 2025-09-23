72094371e8dad3c7b4b34b32070ab7964dfb9cd7
phongnguyen <145427852+phongdz76@users.noreply.github.com>
2025-09-23 15:13:55 +0700

    complete UX nav bar

--- e-commerce/app/components/nav/NavBar.tsx
+++ e-commerce/app/components/nav/NavBar.tsx
@@ -1,5 +1,51 @@
+import Link from "next/link";
+import Container from "../Container";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight: "400" });
+
 const NavBar = () => {
-    return ( <div>NavBar</div> );
-}
- 
+  return (
+    <div
+      className="
+    sticky
+    top-0
+    w-full
+    bg-slate-200
+    z-30
+    shadow-sm"
+    >
+      <div
+        className="
+        py-4
+        border-b-[1px]
+        border-slate-200
+        "
+      >
+        <Container>
+          <div
+            className="
+          flex 
+          items-center 
+          justify-between 
+          gap-3
+          md:gap-0    
+          "
+          >
+            <Link href="/" className={`${redressed.className} text-2xl font-bold text-black`}>
+              <span className="text-black">SG</span>
+              <span className="text-teal-400">Tech</span>
+            </Link>
+            <div>Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
