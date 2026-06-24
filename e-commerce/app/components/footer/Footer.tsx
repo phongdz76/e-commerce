645dae47053a1f77ee80dfd2f9aa011b696e13ad
phongnguyen <145427852+phongdz76@users.noreply.github.com>
2026-06-24 09:06:53 +0700

    feat: add footer component with QR code and company information

--- e-commerce/app/components/footer/Footer.tsx
+++ e-commerce/app/components/footer/Footer.tsx
@@ -94,7 +94,7 @@ const Footer = () => {
             <div className="font-bold text-lg">Follow Us</div>
             <div className="flex flex-col items-start space-y-3">
               <Image
-                src="/Image/qrcode.png"
+                src="/Image/qr_code.png"
                 alt="QR Code - Follow Us"
                 width={120}
                 height={120}
